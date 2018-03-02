0cf95c9895ec4930dbbbc141df25df9043d7e09e
Robert Romaniw <Mamba@Roberts-MacBook-Pro-2.local>
2018-03-02 15:57:28 -0500

    add new message / create message events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+// initiating request - making request from client to server to open web socket and keep that connection open.
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Rob',
+    text: 'Whatup cuh'
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('newMessage',message)
+});
